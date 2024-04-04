c947965ce14bc9489e580f52ba11f05b44b08318
Justin <120177475+JustinL-IxD@users.noreply.github.com>
2024-04-04 09:56:39 +0100

    add check your answers

--- app/getting-in-and-out-of-bed/check-your-answers/page.tsx
+++ app/getting-in-and-out-of-bed/check-your-answers/page.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from 'next';
 
 import PageHeader from '@/components/PageHeader';
 import Wrapper from '@/components/Wrapper';
-import Button from '@/components/Button';
-import ButtonGroup from '@/components/ButtonGroup';
+import SummaryList from '@/components/SummaryList';
 
 export const metadata:Metadata = {
     title: 'Check your answers',
@@ -14,30 +13,54 @@ export const metadata:Metadata = {
  *
  * @returns {JSX.Element} - The page
  */
-const HospitalCareHomeLegalDetention:React.FC = function HospitalCareHomeLegalDetention() {
+const Page:React.FC = function Page() {
     const pageTitle:string = (
         typeof metadata.title === 'string'
             ? metadata.title
             : 'Social Security Scotland'
     );
 
+    const listItems = [
+        {
+            label: 'Do you have difficulty getting in or out of bed?',
+            answer: 'Yes',
+            actions: [{
+                label: 'Change',
+                action: '#',
+            }],
+        },
+        {
+            label: 'Do you ever use any aids, tools or adaptions to get in or out of bed?',
+            answer: 'Yes',
+            actions: [{
+                label: 'Change',
+                action: '#',
+            }],
+        },
+        {
+            label: 'Tell us any difficulty you have getting in and out of bed.',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+            actions: [{
+                label: 'Change',
+                action: '#',
+            }],
+        },
+    ];
+
     return (
         <>
             <Wrapper>
-                <PageHeader title={pageTitle} />
+                <PageHeader
+                    title={pageTitle}
+                />
             </Wrapper>
             <Wrapper>
-                <ButtonGroup>
-                    <Button
-                        icon="chevron_right"
-                        href="hospital-care-home-legal-detention/stay-hospital-care-home-legal-detention"
-                    >
-                        Continue
-                    </Button>
-                </ButtonGroup>
+                <SummaryList
+                    items={listItems}
+                />
             </Wrapper>
         </>
     );
 };
 
-export default HospitalCareHomeLegalDetention;
+export default Page;
