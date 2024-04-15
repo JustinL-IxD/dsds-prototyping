dec779eaa6bd1a613233ba8e9c23e2c43719ed5d
Justin <120177475+JustinL-IxD@users.noreply.github.com>
2024-04-15 11:08:47 +0100

    Update hub page

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,27 @@ const Page:React.FC = function Page() {
             summary: 'You can complete these in any order.',
             sections: [
                 {
+                    id: 'care needs',
+                    label: 'When your care needs started',
+                    summary: 'Tell us about when your care needs started',
+                    status: 'not-started',
+                    link: '/hospital-care-home-legal-detention',
+                },
+                {
+                    id: 'planned-treatment',
+                    label: 'Planned medical treatment',
+                    summary: 'Tell us about medical treatment or surgery you have planned, or that you\'re on the waiting list for',
+                    status: 'not-started',
+                    link: '/hospital-care-home-legal-detention',
+                },
+                {
+                    id: 'conditions-medications',
+                    label: 'Conditions, medications and treatments',
+                    summary: 'Tell us about any conditions or symptoms you have, and any medications you take',
+                    status: 'not-started',
+                    link: '/hospital-care-home-legal-detention',
+                },
+                {
                     id: 'hospital',
                     label: 'Current stays in a hospital, care home or legal detention',
                     summary: "Tell us whether you're currently in hospital, a care home or legal detention",
@@ -48,6 +69,83 @@ const Page:React.FC = function Page() {
                     status: 'not-started',
                     link: 'getting-in-and-out-of-bed',
                 },
+                {
+                    id: 'washing-bathing',
+                    label: 'Washing and bathing',
+                    summary: 'Questions about help you need to wash and bathe, and look after your appearance',
+                    status: 'not-started',
+                    link: 'getting-in-and-out-of-bed',
+                },
+                {
+                    id: 'toilet-needs',
+                    label: 'Toilet needs and managing incontinence',
+                    summary: 'Questions about help you need to use the toilet and manage incontinence',
+                    status: 'not-started',
+                    link: 'getting-in-and-out-of-bed',
+                },
+                {
+                    id: 'moving-indoors',
+                    label: 'Moving around indoors',
+                    summary: 'Questions about help you need to stand and walk around indoors',
+                    status: 'not-started',
+                    link: 'getting-in-and-out-of-bed',
+                },
+                {
+                    id: 'dressing',
+                    label: 'Dressing and undressing',
+                    summary: 'Questions about help you need to get dressed and undressed',
+                    status: 'not-started',
+                    link: 'getting-in-and-out-of-bed',
+                },
+                {
+                    id: 'communicating',
+                    label: 'Communicating with others',
+                    summary: 'Questions about your ability to hear, understand and communicate and any help you need to do this',
+                    status: 'not-started',
+                    link: 'getting-in-and-out-of-bed',
+                },
+                {
+                    id: 'eating-drinking',
+                    label: 'Eating and drinking',
+                    summary: 'Questions about help you need to eat and drink, and any aids or adaptations you use',
+                    status: 'not-started',
+                    link: 'getting-in-and-out-of-bed',
+                },
+                {
+                    id: 'safe-day',
+                    label: 'Staying safe during the day',
+                    summary: 'Questions about help you need to stay safe during the day',
+                    status: 'not-started',
+                    link: 'getting-in-and-out-of-bed',
+                },
+                {
+                    id: 'activities-interests',
+                    label: 'Activities and interests',
+                    summary: 'Questions about help you need to do the activities and interests you like to do during the day',
+                    status: 'not-started',
+                    link: 'getting-in-and-out-of-bed',
+                },
+                {
+                    id: 'support',
+                    label: 'Support taking medication, monitoring a condition and with therapy',
+                    summary: 'Questions about help you need to take medication, monitor a health condition or do treatments at home',
+                    status: 'not-started',
+                    link: 'getting-in-and-out-of-bed',
+                },
+            ],
+            ordered: true,
+        },
+        {
+            label: 'Night care',
+            summary: 'You can complete these in any order.',
+            sections: [
+                {
+                    id: 'night-time-care',
+                    label: 'Night time care needs',
+                    summary: 'Questions about the care or support you need to get settled, sleep comfortably, manage toilet needs and incontinence, and with medication, treatment and therapies',
+                    status: 'not-started',
+                    link: '/hospital-care-home-legal-detention',
+                },
             ],
             ordered: true,
         },
@@ -61,8 +159,8 @@ const Page:React.FC = function Page() {
 
             <Wrapper>
                 <TaskListGroup
-                    heading=""
-                    summary="You need to complete and review each section below before submitting your application."
+                    heading="Part 2"
+                    summary="You should complete the sections in Part 2 by 14 July 2024"
                     ordered
                     groups={groups}
                 />
